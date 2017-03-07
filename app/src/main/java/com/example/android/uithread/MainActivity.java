30645442269a2e1edbb0c758a1f0bc599019b879
Your Name <giyoon21c@gmail.com>
2017-03-07 07:05:58 -0800

    updating textView using asyncTask api

--- app/src/main/java/com/example/android/uithread/MainActivity.java
+++ app/src/main/java/com/example/android/uithread/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.uithread;
 
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -43,11 +44,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.buttonThreadStarter:
                 mStopLoop = true;
 
-                /*
-                while (mStopLoop) {
-                    Log.i(TAG, "Thread id in while loop:" + Thread.currentThread().getId());
-                }
-                */
+                /* The following showed two ways of updating UI from a long running thread
+                0. running it as part of UI thread
+                1. create a separate thread, but can't update the UI element directly
+                2. use handler to post runnable which updates the task
+                3. use textView to post runnable which updates the task
+
+                // 0. This is a bad way of running threads in the UI thread
+                //
+                //while (mStopLoop) {
+                //    Log.i(TAG, "Thread id in while loop:" + Thread.currentThread().getId());
+                //}
 
                 // create a new thread instead of while in the main UI thread
                 new Thread(new Runnable() {
@@ -63,9 +70,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             Log.i(TAG, "Thread id in while loop:" + Thread.currentThread().getId()
                                     + " Thread count: " + count);
 
-                            //cannot update element from UI thread directly.
-                            //threadCountTextView.setText("" + count);
-                            //Instead, use handler to post runnable to the message queue
+                            // 1. cannot update element from UI thread directly.
+                            // threadCountTextView.setText("" + count);
+
+                            // 2. Instead, use handler to post runnable to the message queue
                             handler.post(new Runnable() {
                                 @Override
                                 public void run() {
@@ -73,23 +81,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 }
                             });
 
-                            //instead of using handler, textCountTextView can post a runnable as well
-                            /*
-                            threadCountTextView.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    threadCountTextView.setText("" + count);
-                                }
-                            });
-                            */
+                            // 3. instead of using handler, textCountTextView can post a runnable as well
+                            //
+                            //threadCountTextView.post(new Runnable() {
+                            //    @Override
+                            //    public void run() {
+                            //        threadCountTextView.setText("" + count);
+                            //    }
+                            //});
                         }
                     }
                 }).start();
+
+                */
 
                 break;
             case R.id.buttonStopThread:
                 mStopLoop = false;
                 break;
         }
+    }
+
+    private class MyAsyncTask extends AsyncTask<Integer, Integer, Integer>{
+        private int customCounter;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            customCounter = 0;
+        }
+
+        @Override
+        protected Integer doInBackground(Integer... integers) {
+            customCounter = integers[0];
+            while (mStopLoop) {
+                try {
+                    Thread.sleep(1000);
+                    customCounter++;
+                    publishProgress(customCounter);
+                } catch (InterruptedException e) {
+                    Log.i(TAG, e.getMessage());
+                }
+            }
+            return customCounter;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            threadCountTextView.setText("" + values[0]);
+
+        }
+        
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute();
+            threadCountTextView.setText("" + integer);
+        }
+
     }
 }
